daf4023a0adf3d871ac10c310dcb65f0a0b53246
Anuradha-SNAD <anuradha.avaduthwar@snaddevelopers.com>
2026-07-01 19:26:55 +0530

    Add files via upload

---
+++ model/ProductRequestDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductManagement.DTOs
+{
+    public class ProductRequestDTO
+    {
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Description is required.")]
+        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Price is required.")]
+        [Range(1, 100000)]
+        public decimal Price { get; set; }
+        [Required(ErrorMessage = "Quantity is required.")]
+        [Range(1, 10000)]
+        public int Quantity { get; set; }
+        [Required(ErrorMessage = "Brand is required.")]
+        public string Brand { get; set; }
+        [Required(ErrorMessage = "SKU is required.")]
+        public string SKU { get; set; }
+    }
+}
---
+++ model/ProductResponseDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace ProductManagement.DTOs
+{
+    public class ProductResponseDTO
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Brand { get; set; }
+
+        public string SKU { get; set; }
+    }
+}
