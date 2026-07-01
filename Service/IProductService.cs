f5f72430e53c3ecce9c98e50b3f769a8e306f151
Anuradha-SNAD <anuradha.avaduthwar@snaddevelopers.com>
2026-07-01 19:28:30 +0530

    Add files via upload

---
+++ Service/IProductService.cs
@@ -0,0 +1,26 @@
+﻿using ProductManagement.DTOs;
+
+namespace ProductManagement.Service
+{
+    public interface IProductService
+    {
+        void Add(ProductRequestDTO dto);
+        List<ProductResponseDTO> GetAll();
+
+        ProductResponseDTO GetById(int id);
+
+        void Update(int id, ProductRequestDTO dto);
+
+        void Delete(int id);
+
+        //V2 
+
+        List<ProductResponseDTO> SearchByName(string name);
+        List<ProductResponseDTO> SearchByBrand(string brand);
+        List<ProductResponseDTO> SearchByPrice(decimal minPrice, decimal maxPrice);
+
+        List<ProductResponseDTO> SortByPrice();
+        List<ProductResponseDTO> SortByName();
+        List<ProductResponseDTO> Pagination(int page, int pageSize);
+    }
+}
---
+++ Service/ProductService.cs
@@ -0,0 +1,118 @@
+﻿using ProductManagement.DTOs;
+using ProductManagement.Exceptions;
+using ProductManagement.Repository;
+using System.Xml.Linq;
+
+namespace ProductManagement.Service
+{
+    public class ProductService : IProductService { 
+        private readonly IProductRepository repository;
+
+        public ProductService(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Add(ProductRequestDTO dto)
+        {
+            repository.Add(dto);
+        }
+
+        public void Delete(int id)
+        {
+            var p = repository.GetById(id);
+            if(p == null)
+            {
+               throw new ProductNotFoundException($"Product with Id {id} not found.");
+            }
+            repository.Delete(id);
+        }
+
+        public List<ProductResponseDTO> GetAll()
+        {
+            return repository.GetAll();
+        }
+
+        public ProductResponseDTO GetById(int id)
+        {
+            var p = repository.GetById(id);
+            if(p == null)
+            {
+                throw new ProductNotFoundException($"Product with Id {id} not found.");
+            }
+            return p;
+        }
+
+        public List<ProductResponseDTO> Pagination(int page, int pageSize)
+        {
+            var p = repository.Pagination(page, pageSize);
+            if (!p.Any())
+            {
+                throw new ProductNotFoundException($"NNo products found for the requested page.");
+            }
+            return p;
+        }
+
+        public List<ProductResponseDTO> SearchByBrand(string brand)
+        {
+            var p = repository.SearchByBrand(brand);
+            if (p == null)
+            {
+                throw new ProductNotFoundException($"No products found with name '{brand}'.");
+            }
+            return p;
+        }
+
+        public List<ProductResponseDTO> SearchByName(string name)
+        {
+            var p = repository.SearchByName(name);
+            if(p == null)
+            {
+                throw new ProductNotFoundException($"No products found with name '{name}'.");
+            }
+            return p;
+        }
+
+
+        public List<ProductResponseDTO> SearchByPrice(decimal minPrice, decimal maxPrice)
+        {
+            var p = repository.SearchByPrice(minPrice, maxPrice);
+            if (!p.Any())
+            {
+                throw new ProductNotFoundException($"No products found between ₹{minPrice} and ₹{maxPrice}.");
+            }
+            return p;
+        }
+
+        public List<ProductResponseDTO> SortByName()
+        {
+            var p = repository.SortByName();
+            if (!p.Any())
+            {
+                throw new ProductNotFoundException("No products available.");
+            }
+            return p;
+
+        }
+
+        public List<ProductResponseDTO> SortByPrice()
+        {
+            var p = repository.SortByPrice();
+            if (!p.Any())
+            {
+                throw new ProductNotFoundException("No products available.");
+            }
+            return p;
+        }
+
+        public void Update(int id, ProductRequestDTO dto)
+        {
+            var p = repository.GetById(id);
+            if (p == null)
+            {
+                throw new ProductNotFoundException($"Product with Id {id} not found.");
+            }
+            repository.Update(id, dto);
+        }
+    }
+}
