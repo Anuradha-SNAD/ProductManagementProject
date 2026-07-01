5f2d66cf29d6ad480c5ef3fbee249c7567450ba9
Anuradha-SNAD <anuradha.avaduthwar@snaddevelopers.com>
2026-07-01 19:29:28 +0530

    Add files via upload

---
+++ model/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagement.Model;
+
+namespace ProductManagement.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> products { get; set; }
+             
+    }
+}
